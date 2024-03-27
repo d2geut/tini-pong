8423b441f5c5b424cdacdfdd45643e5872787d4f
d2geut <d2geut@naver.com>
2024-03-27 20:17:58 +0900

    ✨ feat: gameroom 방 정보 레이아웃

--- frontend/src/components/Room.js
+++ frontend/src/components/Room.js
@@ -8,7 +8,7 @@ export default class extends AbstractComponent {
 
 	async getHtml() {
 		return `
-		<div class="container-md text-center">
+		<div class="container-md">
 			<div class="row">
 				<div class="col-7 border p-2">
 					<div class="row row-cols-1 row-cols-md-2 g-4">
@@ -55,8 +55,21 @@ export default class extends AbstractComponent {
 					</div>
 				</div>
 				<div class="col-5 border p-2">
-					<div class="m-auto" style="background-color: #ced4da;" spacing="2.4rem">
-						<h4>TITLE</h4>
+					<div class="m-auto p-3" style="background-color: #ced4da;">
+						<div>
+							<div>
+								<h4>TITLE</h4>
+								<h4>&nbsp</4>
+							</div>
+							<div class="mb-1">
+								<span style="display: inline-block; width: 10rem; font-weight: bold;">Difficulty</span>
+								<span>HARD</span>
+							</div>
+							<div>
+								<span style="display: inline-block; width: 10rem; font-weight: bold;">Type</span>
+								<span>1:1</span>
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>
